f049728b2d4680dac587c0c6e8a18da0d9e7d6a1
Lourran Olimpio Zorzi <achimid@hotmail.com>
2017-07-16 23:07:59 -0300

    First ADM

---
+++ src/main/java/br/com/ecommerce/bff/adm/ecommerceadm/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package br.com.ecommerce.bff.adm.ecommerceadm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Lourran on 15/07/2017.
+ */
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/home");
+    }
+}
