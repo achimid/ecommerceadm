1490556af2c38ecb98accc8496232743a6410c08
Lourran Olimpio Zorzi <achimid@hotmail.com>
2017-07-16 23:11:09 -0300

    First ADM

--- src/main/java/br/com/ecommerce/bff/adm/ecommerceadm/home/HomePage.java
+++ src/main/java/br/com/ecommerce/bff/adm/ecommerceadm/home/HomePage.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class HomePage {
+
     private static final String INDEX_PAGE = "static/index.html";
 
     @GetMapping("/home")
